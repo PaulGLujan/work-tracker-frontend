dfa37757e59df6edc30d270d4f6c9016a5921a16
Paul <paulglujan@gmail.com>
2023-01-15 15:56:40 -0800

    Set up react project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex w-full justify-center rounded border bg-amber-100 p-3 shadow">
+      <h1 className="text-base">Keep on rolling...</h1>
     </div>
   );
 }
